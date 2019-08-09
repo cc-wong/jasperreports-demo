e0b2edda26f1cd5f6c702c1d3bde2eadece05476
Cecilia Wong <wywong@graduate.hku.hk>
2019-08-09 22:44:54 +0800

    Check-in baseline.

---
+++ report-generator/src/main/java/com/asinc/jrdemo/reportgen/JrdemoReportgenApplication.java
@@ -0,0 +1,13 @@
+package com.asinc.jrdemo.reportgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrdemoReportgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrdemoReportgenApplication.class, args);
+	}
+
+}
