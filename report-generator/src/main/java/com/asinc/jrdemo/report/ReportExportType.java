d6e4adf75231cd745c25debbd4f9331484e5d211
Cecilia Wong <wywong@graduate.hku.hk>
2019-08-10 16:28:41 +0800

    Add export types for MS Excel file types.

--- report-generator/src/main/java/com/asinc/jrdemo/report/ReportExportType.java
+++ report-generator/src/main/java/com/asinc/jrdemo/report/ReportExportType.java
@@ -6,13 +6,31 @@ import lombok.Getter;
 /**
  * The report export types.
  */
+
+/**
+ * Instantiates a new report export type.
+ *
+ * @param fileExtension the file extension
+ */
 @AllArgsConstructor
 public enum ReportExportType {
 
 	/** PDF. */
-	PDF("pdf");
+	PDF("pdf"),
+
+	/** MS Office 97. */
+	MS_EXCEL_97("xls"),
+
+	/** MS Office 2003. */
+	MS_EXCEL_2003("xlsx");
 
 	/** The file extension of outputs of this type. */
+
+	/**
+	 * Gets the file extension.
+	 *
+	 * @return the file extension
+	 */
 	@Getter
 	private String fileExtension;
 
