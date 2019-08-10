bb109a0ed2825cf22ae53cf43e8d54e8a0187ca9
Cecilia Wong <wywong@graduate.hku.hk>
2019-08-10 17:50:04 +0800

    Add logging,

--- report-generator/src/main/java/com/asinc/jrdemo/report/service/ReportGenerationService.java
+++ report-generator/src/main/java/com/asinc/jrdemo/report/service/ReportGenerationService.java
@@ -2,6 +2,7 @@ package com.asinc.jrdemo.report.service;
 
 import java.io.File;
 import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
@@ -10,12 +11,14 @@ import com.asinc.jrdemo.report.ReportExportType;
 import com.asinc.jrdemo.report.ReportGenerationProperties;
 import com.asinc.jrdemo.report.engine.ReportEngine;
 
+import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.JRException;
 
 /**
  * The service class for report generation.
  */
 @Service
+@Slf4j
 public class ReportGenerationService {
 
 	/** The report generation properties. */
@@ -36,6 +39,7 @@ public class ReportGenerationService {
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	public File runReport(Document data, ReportExportType exportType) throws JRException, IOException {
+		log.info(String.format("Running report with export type [%s].", exportType));
 		return this.reportEngine.runReport(data, this.reportGenerationProperties, exportType);
 	}
 
